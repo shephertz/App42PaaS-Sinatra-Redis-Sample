f6ab012cbd91528ee1dc7da7c36c8d79aaae4d3c
PravinKShepHertz <pravin.kumar@shephertz.co.in>
2014-01-08 13:34:01 +0530

    added comment in redis configuration file

--- config/redis.rb
+++ config/redis.rb
@@ -1,1 +1,2 @@
-Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
+# Point to your Redis server
+Redis.current = Redis.new(:host => '<VM IP>', :port => <VM PORT>, :password => '<PASSWORD>')
